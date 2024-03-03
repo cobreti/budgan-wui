a931dc55ce47079930d05ecaf70e60047bf6d467
Danny T <cobreti_@hotmail.com>
2024-03-03 18:11:10 -0500

    added statement loading

--- src/stores/add-statement-store.ts
+++ src/stores/add-statement-store.ts
@@ -1,8 +1,12 @@
 import {defineStore} from 'pinia';
 import {ref, type Ref} from 'vue';
-import type {BankAccount} from '@models/BankAccountTypes';
+import type {BankAccount, BankAccountTransaction, TransactionIdsTable} from '@models/BankAccountTypes';
 import {timer} from 'rxjs';
 import {resolve} from 'inversify/lib/resolution/resolver';
+import type {IOfxParser} from '@/services/ofxParser';
+import {ServicesTypes} from '@/services/types';
+import {container} from '@/setupInversify';
+import type {OfxDocument, OfxTransaction} from '@models/ofxDocument';
 
 export declare type LoadedAccount = {
     loading: boolean,
@@ -27,18 +31,88 @@ export const useAddStatementStore = defineStore<string, AddStatementStore>('addS
             account: undefined
         };
 
-        timer(5000).subscribe(() => {
+        const reader = new FileReader();
+
+        reader.onload = () => onOfxLoaded(reader.result as string);
+
+        reader.readAsText(file);
+    }
+
+    function onOfxLoaded(content: string) {
+        const ofxParser : IOfxParser = container.get(ServicesTypes.OfxParser);
+
+        if (ofxParser) {
+            const result = ofxParser.parse(content);
+
+            if (result.document) {
+                loadAccount(result.document);
+            }
+        }
+    }
+
+    function OfxToBankAccountTransaction(ofxTransaction: OfxTransaction) : BankAccountTransaction {
+        if (ofxTransaction.fitId == undefined) {
+            throw new Error('Transaction ID not found in OFX file.');
+        }
+
+        if (ofxTransaction.datePosted == undefined) {
+            throw new Error('Transaction date not found in OFX file.');
+        }
+
+        if (ofxTransaction.amount == undefined) {
+            throw new Error('Transaction amount not found in OFX file.');
+        }
+
+        if (ofxTransaction.type == undefined) {
+            throw new Error('Transaction type not found in OFX file.');
+        }
+
+        return {
+            transactionId: ofxTransaction.fitId,
+            date: ofxTransaction.datePosted,
+            amount: ofxTransaction.amount,
+            type: ofxTransaction.type,
+            description: ofxTransaction.name || ''
+        }
+    }
+
+    function loadAccount(document: OfxDocument) {
+        if (document.accountId == undefined) {
+            throw new Error('Account ID not found in OFX file.');
+        }
+
+        if (document.startDate == undefined) {
+            throw new Error('Start date not found in OFX file.');
+        }
+
+        if (document.endDate == undefined) {
+            throw new Error('End date not found in OFX file.');
+        }
+
+        if (document.transactions && document.transactions.length > 0) {
+
+            const bankAccountTransactions = document.transactions.map(OfxToBankAccountTransaction);
+
+            const account : BankAccount = {
+                accountId: document.accountId,
+                accountType: document.accountType || '',
+                transactionsId: bankAccountTransactions.map(transaction => transaction.transactionId)
+                    .reduce((acc: TransactionIdsTable, id) => {
+                        acc[id] = undefined;
+                        return acc;
+                    }, {}),
+                transactions: [{
+                    dateStart: document.startDate,
+                    dateEnd: document.endDate,
+                    transactions: bankAccountTransactions
+                }]
+            }
+
             loadedAccount.value = {
                 loading: false,
-                account: {
-                    accountId: '123',
-                    accountType: 'checking',
-                    transactionsId: {},
-                    transactions: []
-                }
-            };
-        });
-
+                account: account
+            }
+        }
     }
 
     return {
