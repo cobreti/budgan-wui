008c84c1870bc89361270641907e37fb55a2fea5
Danny Thibaudeau <cobreti_@hotmail.com>
2024-07-17 19:45:32 -0400

    Add version (#3)
    * added version to appSettings
    
    * display version at top right

--- src/stores/appSettings-store.ts
+++ src/stores/appSettings-store.ts
@@ -3,22 +3,34 @@ import { type Ref, ref } from 'vue'
 
 export type AppSettings = {
   drawerVisible: boolean;
+  version: string;
 }
 
 
 export type AppSettingsStore = {
   appSettings: Ref<AppSettings>;
+
+  setVersion(version: string): void;
 }
 
 
 export const useAppSettingsStore = defineStore<string, AppSettingsStore>('appSettings', () => {
 
   const appSettings = ref({
-    drawerVisible: true
+    drawerVisible: true,
+    version: '0.0.0.0'
   })
 
+  function setVersion(version: string) {
+    appSettings.value = {
+      ...appSettings.value,
+      version
+    };
+  }
+
   return {
-    appSettings
+    appSettings,
+    setVersion
   };
 });
 
