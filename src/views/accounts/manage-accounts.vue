c8d0ab2d99d06b4179402756ac845b1e268b594c
Danny T <cobreti_@hotmail.com>
2024-08-10 22:34:42 -0400

    added form to create a new account

--- src/views/accounts/manage-accounts.vue
+++ src/views/accounts/manage-accounts.vue
@@ -3,8 +3,32 @@
     <v-toolbar>
       <v-btn flat :to="{path: '/accounts/new'}">New Account</v-btn>
     </v-toolbar>
-    <h1>Manage Accounts</h1>
-    <p>Accounts page content</p>
+    <div class="ma-5">
+      <h1>Accounts</h1>
+      <ul>
+        <li v-for="account in accounts" :key="account.accountId">
+          <v-card>
+            <v-card-title>{{account.accountId}}</v-card-title>
+            <v-card-text>{{account.name}}</v-card-text>
+          </v-card>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
+<style scoped>
+</style>
+
+<script setup lang="ts">
+
+  import { useBankAccountsStore } from '@/stores/bankAccounts-store'
+  import { computed } from 'vue'
+
+  const bankAccountStore = useBankAccountsStore();
+
+  const accounts = computed(() => {
+    return Object.values(bankAccountStore.accounts);
+  });
+
+</script>
--- src/views/accounts/new-account.vue
+++ src/views/accounts/new-account.vue
@@ -8,18 +8,23 @@
       </a>
     </v-toolbar>
     <div class="ma-5 form">
-      <v-form>
-        <v-text-field
-          label="Account Name"
-          required
-          v-model="accountName"></v-text-field>
+      <v-form fast-fail @submit.prevent v-model="formValid">
         <v-text-field
           label="Account Number"
           required
+          :rules = "accountNumberRules"
           v-model="accountNumber"></v-text-field>
+        <v-text-field
+          label="Account Name"
+          required
+          :rules = "accountNameRules"
+          v-model="accountName"></v-text-field>
         <v-btn
+          type="submit"
           @click="createAccount"
-          color="primary">Create Account</v-btn>
+          color="primary"
+          :disabled="!formValid"
+          block>Create Account</v-btn>
       </v-form>
     </div>
   </div>
@@ -37,11 +42,47 @@
 
 <script setup lang="ts">
 
-  const accountName = defineModel('accountName', {required: true});
-  const accountNumber = defineModel('accountNumber');
+  import { useBankAccountsStore } from '@/stores/bankAccounts-store'
+  import type { BankAccount } from '@models/BankAccountTypes'
+  import router from '@/router'
+
+  const bankAccountStore = useBankAccountsStore();
+
+  const accountName = defineModel<string>('accountName', {required: true});
+  const accountNumber = defineModel<string>('accountNumber', {required: true});
+  const formValid = defineModel<boolean>('formValid');
+
+  const accountNameRules = [
+    (v: string) => {
+      if (!v) return 'Account name is required';
+
+      return true;
+    },
+  ];
+
+  const accountNumberRules = [
+    (v: string) => {
+
+      if (!v) return 'Account number is required';
+
+      const account = bankAccountStore.getAccountByIdIfExist(v);
+      if (account) return 'Account number already exists';
+
+      return true;
+    },
+  ]
 
   async function createAccount() {
-    console.log('test');
+    const bankAccount : BankAccount = {
+      name: accountName.value,
+      accountId: accountNumber.value,
+      accountType: "",
+      transactionsGroups: []
+    };
+
+    bankAccountStore.addWithBankAccount(bankAccount);
+
+    router.go(-1);
   }
 
 </script>
