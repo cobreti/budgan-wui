25e778cf8e41c8e355024e99dfd902406190571f
Danny T <cobreti_@hotmail.com>
2025-04-25 18:13:53 -0400

    added delete account button

--- src/stores/bankAccounts-store.ts
+++ src/stores/bankAccounts-store.ts
@@ -1,179 +1,205 @@
 import { defineStore } from 'pinia'
 import { computed, type ComputedRef, type Ref, ref } from 'vue'
 import type {
-    BankAccount, BankAccountsDictionary,
+    BankAccount,
+    BankAccountsDictionary,
     BankAccountTransaction,
     BankAccountTransactionsGroup,
     TransactionIdsTable
-} from '@models/BankAccountTypes';
+} from '@models/BankAccountTypes'
 
-type BankAccountsTransactionIdsIndex = { [accountId: string]: TransactionIdsTable };
+type BankAccountsTransactionIdsIndex = { [accountId: string]: TransactionIdsTable }
 
 export type BankAccountsStore = {
-    accounts: Ref<BankAccountsDictionary>;
-    transactionsIdsIndex: Ref<BankAccountsTransactionIdsIndex>;
-    totalTransactionsPerAccount: ComputedRef<{ [key: string]: number }>;
-    totalTransactionIdsPerAccount: ComputedRef<{ [key: string]: number }>;
-    getAccountById: (accountId: string) => BankAccount;
-    getAccountByNameIfExists: (accountName: string) => BankAccount | undefined;
-    getAccountByIdIfExist: (accountId: string) => BankAccount | undefined;
-    addWithBankAccount: (account: BankAccount, accountId?: string) => void;
-    hasAccounts: ComputedRef<boolean>;
-    clear: () => void;
-    getTransactionsIdsForAccountId: (accountId: string) => TransactionIdsTable;
+    accounts: Ref<BankAccountsDictionary>
+    transactionsIdsIndex: Ref<BankAccountsTransactionIdsIndex>
+    totalTransactionsPerAccount: ComputedRef<{ [key: string]: number }>
+    totalTransactionIdsPerAccount: ComputedRef<{ [key: string]: number }>
+    getAccountById: (accountId: string) => BankAccount
+    getAccountByNameIfExists: (accountName: string) => BankAccount | undefined
+    getAccountByIdIfExist: (accountId: string) => BankAccount | undefined
+    addWithBankAccount: (account: BankAccount, accountId?: string) => void
+    hasAccounts: ComputedRef<boolean>
+    clear: () => void
+    getTransactionsIdsForAccountId: (accountId: string) => TransactionIdsTable
+    removeAccount: (accountId: string) => void
 }
 
-export const useBankAccountsStore = defineStore<string, BankAccountsStore>('bankTransactions', () => {
+export const useBankAccountsStore = defineStore<string, BankAccountsStore>(
+    'bankTransactions',
+    () => {
+        const accounts = ref<BankAccountsDictionary>({})
+        const transactionsIdsIndex = ref<BankAccountsTransactionIdsIndex>({})
 
-    const accounts = ref<BankAccountsDictionary>({});
-    const transactionsIdsIndex = ref<BankAccountsTransactionIdsIndex>({});
+        const hasAccounts = computed(() => {
+            return Object.keys(accounts.value).length > 0
+        })
 
-    const hasAccounts = computed(() => {
-        return Object.keys(accounts.value).length > 0;
-    });
-
-    const totalTransactionsPerAccount: ComputedRef<{ [key: string]: number }> = computed(() => {
-        return Object.values(accounts.value)
-            .map((account) => {
-                const count = account.transactionsGroups.map((transactionsGroup) => {
-                    return transactionsGroup.transactions.length;
+        const totalTransactionsPerAccount: ComputedRef<{ [key: string]: number }> = computed(() => {
+            return Object.values(accounts.value)
+                .map((account) => {
+                    const count = account.transactionsGroups
+                        .map((transactionsGroup) => {
+                            return transactionsGroup.transactions.length
+                        })
+                        .reduce((acc, count) => acc + count, 0)
+                    return { [account.accountId]: count }
                 })
-                    .reduce((acc, count) => acc + count, 0);
-                return { [account.accountId]: count }
-            })
-            .reduce((acc, count) => {
-                return { ...acc, ...count };
-            }, {});
-    });
+                .reduce((acc, count) => {
+                    return { ...acc, ...count }
+                }, {})
+        })
 
-    const totalTransactionIdsPerAccount = computed(() => {
-        return Object.keys(transactionsIdsIndex.value)
-            .map((accountId) => {
-                return { [accountId]: Object.keys(transactionsIdsIndex.value[accountId]).length };
-            })
-            .reduce((acc, count) => {
-                return { ...acc, ...count };
-            }, {});
-    });
+        const totalTransactionIdsPerAccount = computed(() => {
+            return Object.keys(transactionsIdsIndex.value)
+                .map((accountId) => {
+                    return {
+                        [accountId]: Object.keys(transactionsIdsIndex.value[accountId]).length
+                    }
+                })
+                .reduce((acc, count) => {
+                    return { ...acc, ...count }
+                }, {})
+        })
 
-    function getAccountById(accountId: string) {
-        return accounts.value[accountId];
-    }
-
-    function getAccountByNameIfExists(accountName: string): BankAccount | undefined {
-        const accountsInfo = Object.values(accounts.value);
-
-        return accountsInfo.find((account) => account.name === accountName);
-    }
-
-    function getAccountByIdIfExist(accountId: string) {
-        if (accountId in accounts.value) {
-            return accounts.value[accountId];
+        function getAccountById(accountId: string) {
+            return accounts.value[accountId]
         }
-        return undefined;
-    }
 
-    function sanitizeTransactionsGroup(accountTransactionsIds: TransactionIdsTable, transactionsGroup: BankAccountTransactionsGroup) {
+        function getAccountByNameIfExists(accountName: string): BankAccount | undefined {
+            const accountsInfo = Object.values(accounts.value)
 
-        const newTransactions = transactionsGroup.transactions.filter((transaction) => {
-            return !(transaction.transactionId in accountTransactionsIds);
-        });
+            return accountsInfo.find((account) => account.name === accountName)
+        }
+
+        function getAccountByIdIfExist(accountId: string) {
+            if (accountId in accounts.value) {
+                return accounts.value[accountId]
+            }
+            return undefined
+        }
+
+        function sanitizeTransactionsGroup(
+            accountTransactionsIds: TransactionIdsTable,
+            transactionsGroup: BankAccountTransactionsGroup
+        ) {
+            const newTransactions = transactionsGroup.transactions.filter((transaction) => {
+                return !(transaction.transactionId in accountTransactionsIds)
+            })
+
+            return {
+                ...transactionsGroup,
+                dateStart: transactionsGroup.dateStart,
+                dateEnd: transactionsGroup.dateEnd,
+                transactions: newTransactions
+            } as BankAccountTransactionsGroup
+        }
+
+        function addWithBankAccount(account: BankAccount, accountId?: string) {
+            const targetAccountId = accountId || account.accountId
+            const existingAccount = getAccountById(targetAccountId)
+            const accountTransactionsIds = transactionsIdsIndex.value[targetAccountId] || {}
+
+            if (!existingAccount) {
+                accounts.value[targetAccountId] = account
+            } else {
+                const sanitizedTransactionsGroup = account.transactionsGroups
+                    .map((transactionsGroup) => {
+                        return sanitizeTransactionsGroup(accountTransactionsIds, transactionsGroup)
+                    })
+                    .filter((transactionsGroup) => transactionsGroup.transactions.length > 0)
+
+                if (sanitizedTransactionsGroup.length > 0) {
+                    existingAccount.transactionsGroups = [
+                        ...existingAccount.transactionsGroups,
+                        ...sanitizedTransactionsGroup
+                    ]
+                }
+            }
+
+            const ids = buildTransactionIdsForAccount(account)
+
+            transactionsIdsIndex.value[targetAccountId] = { ...accountTransactionsIds, ...ids }
+        }
+
+        function buildTransactionIdsForAccount(account: BankAccount): TransactionIdsTable {
+            return account.transactionsGroups
+                .map((transactionsGroup) => {
+                    return transactionsGroup.transactions
+                        .map((transaction) => {
+                            return transaction.transactionId
+                        })
+                        .reduce((acc: TransactionIdsTable, id) => {
+                            acc[id] = {}
+                            return acc
+                        }, {})
+                })
+                .reduce((acc: TransactionIdsTable, ids) => {
+                    return { ...acc, ...ids }
+                }, {})
+        }
+
+        function getTransactionsIdsForAccountId(accountId: string): TransactionIdsTable {
+            if (!(accountId in transactionsIdsIndex.value)) {
+                return {}
+            }
+
+            return transactionsIdsIndex.value[accountId]
+        }
+
+        function removeAccount(accountId: string): void {
+            if (accountId in accounts.value) {
+                delete accounts.value[accountId]
+                delete transactionsIdsIndex.value[accountId]
+            }
+        }
+
+        function clear() {
+            accounts.value = {}
+        }
 
         return {
-            ...transactionsGroup,
-            dateStart: transactionsGroup.dateStart,
-            dateEnd: transactionsGroup.dateEnd,
-            transactions: newTransactions
-        } as BankAccountTransactionsGroup;
-    }
-
-    function addWithBankAccount(account: BankAccount, accountId?: string) {
-
-        const targetAccountId = accountId || account.accountId;
-        const existingAccount = getAccountById(targetAccountId);
-        const accountTransactionsIds = transactionsIdsIndex.value[targetAccountId] || {};
-
-        if (!existingAccount) {
-            accounts.value[targetAccountId] = account;
+            accounts,
+            transactionsIdsIndex,
+            totalTransactionsPerAccount,
+            totalTransactionIdsPerAccount,
+            getAccountById,
+            getAccountByNameIfExists,
+            getAccountByIdIfExist,
+            addWithBankAccount,
+            hasAccounts,
+            clear,
+            getTransactionsIdsForAccountId,
+            removeAccount
         }
-        else {
-            const sanitizedTransactionsGroup = account.transactionsGroups.map((transactionsGroup) => {
-                return sanitizeTransactionsGroup(accountTransactionsIds, transactionsGroup);
-            })
-                .filter((transactionsGroup) => transactionsGroup.transactions.length > 0);
+    },
+    {
+        persist: {
+            storage: sessionStorage,
+            afterRestore(context) {
+                const accounts = context.store.accounts
 
-            if (sanitizedTransactionsGroup.length > 0) {
-                existingAccount.transactionsGroups = [...existingAccount.transactionsGroups, ...sanitizedTransactionsGroup];
-            }
-        }
-
-        const ids = buildTransactionIdsForAccount(account);
-
-        transactionsIdsIndex.value[targetAccountId] = { ...accountTransactionsIds, ...ids };
-    }
-
-    function buildTransactionIdsForAccount(account: BankAccount): TransactionIdsTable {
-        return account.transactionsGroups.map((transactionsGroup) => {
-            return transactionsGroup.transactions.map((transaction) => {
-                return transaction.transactionId;
-            })
-                .reduce((acc: TransactionIdsTable, id) => {
-                    acc[id] = {};
-                    return acc;
-                }, {});
-        })
-            .reduce((acc: TransactionIdsTable, ids) => {
-                return { ...acc, ...ids };
-            }, {});
-    }
-
-    function getTransactionsIdsForAccountId(accountId: string): TransactionIdsTable {
-        if (!(accountId in transactionsIdsIndex.value)) {
-            return {};
-        }
-
-        return transactionsIdsIndex.value[accountId];
-    }
-
-    function clear() {
-        accounts.value = {};
-    }
-
-    return {
-        accounts,
-        transactionsIdsIndex,
-        totalTransactionsPerAccount,
-        totalTransactionIdsPerAccount,
-        getAccountById,
-        getAccountByNameIfExists,
-        getAccountByIdIfExist,
-        addWithBankAccount,
-        hasAccounts,
-        clear,
-        getTransactionsIdsForAccountId
-    }
-}, {
-    persist: {
-        storage: sessionStorage,
-        afterRestore(context) {
-            const accounts = context.store.accounts;
-
-            for (const accountId in accounts) {
-                const account = accounts[accountId];
-                account.transactionsGroups = account.transactionsGroups.map((transactionsGroup: BankAccountTransactionsGroup) => {
-                    return {
-                        ...transactionsGroup,
-                        dateStart: new Date(transactionsGroup.dateStart),
-                        dateEnd: new Date(transactionsGroup.dateEnd),
-                        transactions: transactionsGroup.transactions.map((transaction: BankAccountTransaction) => {
+                for (const accountId in accounts) {
+                    const account = accounts[accountId]
+                    account.transactionsGroups = account.transactionsGroups.map(
+                        (transactionsGroup: BankAccountTransactionsGroup) => {
                             return {
-                                ...transaction,
-                                date: new Date(transaction.dateInscription)
-                            };
-                        })
-                    };
-                });
+                                ...transactionsGroup,
+                                dateStart: new Date(transactionsGroup.dateStart),
+                                dateEnd: new Date(transactionsGroup.dateEnd),
+                                transactions: transactionsGroup.transactions.map(
+                                    (transaction: BankAccountTransaction) => {
+                                        return {
+                                            ...transaction,
+                                            date: new Date(transaction.dateInscription)
+                                        }
+                                    }
+                                )
+                            }
+                        }
+                    )
+                }
             }
         }
     }
-});
+)
--- src/views/accountsManagement/BdgAccountTile.vue
+++ src/views/accountsManagement/BdgAccountTile.vue
@@ -1,53 +1,62 @@
 <template>
-  <v-card class="ma-10">
-    <v-card-title class="cursor-pointer">
-      <v-btn
-        class="account-name"
-        variant="plain"
-        :to="{path: '/account/' + props.account.accountId + '/transactions'}"
-        >
-        {{props.account.name}}
-      </v-btn>
-    </v-card-title>
-    <v-card-text class="cursor-default ml-6">
-      <div class="d-flex flex-row">
-        <div class="info-span">
-            {{props.account.accountId}}
-        </div>
-        <div class="info-span">{{props.account.accountType}}</div>
-      </div>
-    </v-card-text>
-  </v-card>
+    <v-card class="ma-10">
+        <v-card-title class="cursor-pointer">
+            <v-btn
+                class="account-name"
+                variant="plain"
+                :to="{ path: '/account/' + props.account.accountId + '/transactions' }"
+            >
+                {{ props.account.name }}
+            </v-btn>
+        </v-card-title>
+        <v-card-text class="cursor-default ml-6">
+            <div class="d-flex flex-row">
+                <div class="info-span">
+                    {{ props.account.accountId }}
+                </div>
+                <div class="info-span">{{ props.account.accountType }}</div>
+                <v-btn class="ml-auto" color="red" @click="deleteAccount(props.account.accountId)">
+                    Delete
+                </v-btn>
+            </div>
+        </v-card-text>
+    </v-card>
 </template>
 
 <style scoped>
+    .account-name {
+        font-size: 1em;
+        font-weight: bold;
+    }
 
-  .account-name {
-    font-size: 1em;
-    font-weight: bold;
-  }
+    .cursor-default {
+        cursor: default;
+    }
 
-  .cursor-default {
-    cursor: default;
-  }
+    .cursor-pointer {
+        cursor: pointer;
+    }
 
-  .cursor-pointer {
-    cursor: pointer;
-  }
-
-  .info-span {
-    margin-right: 1em;
-    width: 20em;
-  }
+    .info-span {
+        margin-right: 1em;
+        width: 20em;
+    }
 </style>
 
 <script setup lang="ts">
+    import { type BankAccount } from '@models/BankAccountTypes'
+    import { useBankAccountsStore } from '@/stores/bankAccounts-store'
 
-  import { type BankAccount } from '@models/BankAccountTypes'
+    const props = defineProps<{
+        account: BankAccount
+    }>()
 
-  const props = defineProps<{
-    account: BankAccount
-  }>();
+    const bankAccountsStore = useBankAccountsStore()
 
-
+    const deleteAccount = (accountId: string) => {
+        if (confirm('Are you sure you want to delete this account?')) {
+            bankAccountsStore.removeAccount(accountId)
+            console.log(`Account with ID ${accountId} deleted.`)
+        }
+    }
 </script>
