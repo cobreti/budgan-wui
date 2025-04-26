fb27e40c1ab5fc06453abda3a4786de6eae51d50
Danny T <cobreti_@hotmail.com>
2025-04-26 18:39:54 -0400

    use column mapping from csv settings when loading

--- src/core/services/BankAccountLoader.ts
+++ src/core/services/BankAccountLoader.ts
@@ -1,157 +1,168 @@
-import { type IOfxToBankAccount } from './OfxToBankAccount';
-import { inject, injectable } from 'inversify';
-import 'reflect-metadata';
-import { ServicesTypes } from './types';
-import type { BankAccount, BankAccountsDictionary } from '@models/BankAccountTypes';
-import type { IBankAccountOperations } from './BankAccountOperations';
-import type { BankAccountTransactionsSanitizerFactory } from './BankAccountTransactionsSanitizerFactory';
+import { type IOfxToBankAccount } from './OfxToBankAccount'
+import { inject, injectable } from 'inversify'
+import 'reflect-metadata'
+import { ServicesTypes } from './types'
+import type { BankAccount, BankAccountsDictionary } from '@models/BankAccountTypes'
+import type { IBankAccountOperations } from './BankAccountOperations'
+import type { BankAccountTransactionsSanitizerFactory } from './BankAccountTransactionsSanitizerFactory'
 import type { ICsvToBankAccount } from '@services/CsvToBankAccount'
 import type { IStreamFactory } from '@services/StreamFactory'
 import type { ICsvParser } from '@services/CsvParser'
-import { CSVColumnContent, type CSVColumnContentMapping } from '@models/csvDocument'
+import { type CSVColumnContentMapping } from '@models/csvDocument'
 
-
-export type BankAccountListById = {[id: string]: BankAccount[]};
-
+export type BankAccountListById = { [id: string]: BankAccount[] }
 
 export interface IBankAccountLoader {
+    loadingFileStarted: BankAccountLoader_LoadingFileStarted | undefined
+    accountLoaded: BankAccountLoader_AccountLoaded | undefined
 
-    loadingFileStarted : BankAccountLoader_LoadingFileStarted | undefined;
-    accountLoaded : BankAccountLoader_AccountLoaded | undefined;
+    load(files: File[]): Promise<void>
+    loadWithAccount(
+        account: BankAccount,
+        mapping: CSVColumnContentMapping,
+        files: File[]
+    ): Promise<void>
+    sanitize(accounts: BankAccountsDictionary): void
 
-    load(files: File[]) : Promise<void>;
-    loadWithAccount(account: BankAccount, files: File[]) : Promise<void>;
-    sanitize(accounts: BankAccountsDictionary) : void;
-
-    get accountsById(): BankAccountsDictionary;
+    get accountsById(): BankAccountsDictionary
 }
 
-
-export type BankAccountLoader_LoadingFileStarted = (filename: string) => void;
-export type BankAccountLoader_AccountLoaded = (account: BankAccount) => void;
-export type BankAccountLoader_AccountLoadError = (filename: string, error: unknown) => void;
-
+export type BankAccountLoader_LoadingFileStarted = (filename: string) => void
+export type BankAccountLoader_AccountLoaded = (account: BankAccount) => void
+export type BankAccountLoader_AccountLoadError = (filename: string, error: unknown) => void
 
 @injectable()
 export class BankAccountLoader implements IBankAccountLoader {
+    rawAccountsLoadedById: BankAccountListById = {}
+    sanitizedAccountsById: BankAccountsDictionary = {}
 
-    rawAccountsLoadedById: BankAccountListById = {};
-    sanitizedAccountsById: BankAccountsDictionary = {};
-
-    loadingFileStarted : BankAccountLoader_LoadingFileStarted | undefined;
-    accountLoaded : BankAccountLoader_AccountLoaded | undefined;
-    accountLoadError : BankAccountLoader_AccountLoadError | undefined;
+    loadingFileStarted: BankAccountLoader_LoadingFileStarted | undefined
+    accountLoaded: BankAccountLoader_AccountLoaded | undefined
+    accountLoadError: BankAccountLoader_AccountLoadError | undefined
 
     constructor(
         @inject(ServicesTypes.OfxToBankAccount) private ofxToBankAccount: IOfxToBankAccount,
         @inject(ServicesTypes.CsvToBankAccount) private csvToBankAccount: ICsvToBankAccount,
-        @inject(ServicesTypes.BankAccountOperations) private bankAccountOperations: IBankAccountOperations,
-        @inject(ServicesTypes.BankAccountTransactionsSanitizerFactory) private bankAccountTransactionsSanitizerFactory: BankAccountTransactionsSanitizerFactory,
+        @inject(ServicesTypes.BankAccountOperations)
+        private bankAccountOperations: IBankAccountOperations,
+        @inject(ServicesTypes.BankAccountTransactionsSanitizerFactory)
+        private bankAccountTransactionsSanitizerFactory: BankAccountTransactionsSanitizerFactory,
         @inject(ServicesTypes.StreamFactory) private streamFactory: IStreamFactory,
         @inject(ServicesTypes.CsvParser) private csvParser: ICsvParser
-    ) {
-        
-    }
+    ) {}
 
-    get accountsById() : BankAccountsDictionary {
-        return this.sanitizedAccountsById;
+    get accountsById(): BankAccountsDictionary {
+        return this.sanitizedAccountsById
     }
 
     public async load(files: File[]) {
         for (const file of files) {
-            this.loadingFileStarted && this.loadingFileStarted(file.name);
-      
+            this.loadingFileStarted && this.loadingFileStarted(file.name)
+
             try {
-                const account = await this.loadFile(file);
+                const account = await this.loadFile(file)
                 if (account.transactionsGroups.length > 0) {
-                    account.transactionsGroups[0].filename = file.name;
+                    account.transactionsGroups[0].filename = file.name
                 }
 
-                const accountId = account.accountId;
+                const accountId = account.accountId
 
-                const accountsForId = this.rawAccountsLoadedById[accountId] || [];
-                accountsForId.push(account);
-                this.rawAccountsLoadedById[accountId] = accountsForId;
+                const accountsForId = this.rawAccountsLoadedById[accountId] || []
+                accountsForId.push(account)
+                this.rawAccountsLoadedById[accountId] = accountsForId
 
-                this.accountLoaded && this.accountLoaded(account);
+                this.accountLoaded && this.accountLoaded(account)
             } catch (error) {
-                this.accountLoadError && this.accountLoadError(file.name, error);
+                this.accountLoadError && this.accountLoadError(file.name, error)
             }
         }
     }
 
-    public async loadWithAccount(account: BankAccount, files: File[]) {
+    public async loadWithAccount(
+        account: BankAccount,
+        csvMapping: CSVColumnContentMapping,
+        files: File[]
+    ) {
         for (const file of files) {
-            this.loadingFileStarted && this.loadingFileStarted(file.name);
+            this.loadingFileStarted && this.loadingFileStarted(file.name)
 
             try {
-                const resultAccount = await this.loadFileWithAccount(account, file);
+                const resultAccount = await this.loadFileWithAccount(account, csvMapping, file)
                 if (resultAccount.transactionsGroups.length > 0) {
-                    resultAccount.transactionsGroups[0].filename = file.name;
+                    resultAccount.transactionsGroups[0].filename = file.name
                 }
 
-                const accountId = resultAccount.accountId;
+                const accountId = resultAccount.accountId
 
-                const accountsForId = this.rawAccountsLoadedById[accountId] || [];
-                accountsForId.push(resultAccount);
-                this.rawAccountsLoadedById[accountId] = accountsForId;
+                const accountsForId = this.rawAccountsLoadedById[accountId] || []
+                accountsForId.push(resultAccount)
+                this.rawAccountsLoadedById[accountId] = accountsForId
 
-                this.accountLoaded && this.accountLoaded(resultAccount);
-            }
-            catch (error) {
-                this.accountLoadError && this.accountLoadError(file.name, error);
+                this.accountLoaded && this.accountLoaded(resultAccount)
+            } catch (error) {
+                this.accountLoadError && this.accountLoadError(file.name, error)
             }
         }
     }
 
-    public async loadFile(file: File)  : Promise<BankAccount> {
-        const reOfx = /\.ofx$/i;
-        const reCsv = /\.csv$/i;
+    public async loadFile(file: File): Promise<BankAccount> {
+        const reOfx = /\.ofx$/i
+        const reCsv = /\.csv$/i
 
         if (reOfx.test(file.name)) {
-            return await this.ofxToBankAccount.loadOfxFile(file);
+            return await this.ofxToBankAccount.loadOfxFile(file)
         } else if (reCsv.test(file.name)) {
-            return await this.csvToBankAccount.loadCsvFile(file);
+            return await this.csvToBankAccount.loadCsvFile(file)
         } else {
-            throw new Error('Unsupported file type');
+            throw new Error('Unsupported file type')
         }
     }
 
-    public async loadFileWithAccount(account: BankAccount, file: File) : Promise<BankAccount> {
-        const reOfx = /\.ofx$/i;
-        const reCsv = /\.csv$/i;
+    public async loadFileWithAccount(
+        account: BankAccount,
+        csvMapping: CSVColumnContentMapping,
+        file: File
+    ): Promise<BankAccount> {
+        const reOfx = /\.ofx$/i
+        const reCsv = /\.csv$/i
 
         if (reOfx.test(file.name)) {
-            return await this.ofxToBankAccount.loadOfxFile(file);
+            return await this.ofxToBankAccount.loadOfxFile(file)
         } else if (reCsv.test(file.name)) {
-            return this.csvFileToBankAccount(account, file);
+            return this.csvFileToBankAccount(account, csvMapping, file)
         } else {
-            throw new Error('Unsupported file type');
+            throw new Error('Unsupported file type')
         }
     }
 
-    public async csvFileToBankAccount(account: BankAccount, file: File) : Promise<BankAccount> {
+    public async csvFileToBankAccount(
+        account: BankAccount,
+        csvMapping: CSVColumnContentMapping,
+        file: File
+    ): Promise<BankAccount> {
+        const inputStream = this.streamFactory.createFileReader(file)
 
-        const inputStream = this.streamFactory.createFileReader(file);
+        const text = await inputStream.read()
 
-        const text = await inputStream.read();
+        this.csvParser.minimumColumnsCount = 3
+        const csvResult = this.csvParser.parse(text)
 
-        this.csvParser.minimumColumnsCount = 3;
-        const csvResult = this.csvParser.parse(text);
+        // const csvColumnsMapping: CSVColumnContentMapping = {
+        //     [CSVColumnContent.CARD_NUMBER]: 0,
+        //     [CSVColumnContent.TYPE]: 1,
+        //     [CSVColumnContent.DATE_INSCRIPTION]: 2,
+        //     [CSVColumnContent.AMOUNT]: 3,
+        //     [CSVColumnContent.DESCRIPTION]: 4,
+        //     [CSVColumnContent.DATE_TRANSACTION]: null
+        // }
 
-        const csvColumnsMapping: CSVColumnContentMapping = {
-            [CSVColumnContent.CARD_NUMBER]: 0,
-            [CSVColumnContent.TYPE]: 1,
-            [CSVColumnContent.DATE_INSCRIPTION]: 2,
-            [CSVColumnContent.AMOUNT]: 3,
-            [CSVColumnContent.DESCRIPTION]: 4,
-            [CSVColumnContent.DATE_TRANSACTION]: null
-        };
-
-        const transactionsGroup = this.csvToBankAccount.convertToBankAccountTransactionsGroup(csvResult.content, csvColumnsMapping);
+        const transactionsGroup = this.csvToBankAccount.convertToBankAccountTransactionsGroup(
+            csvResult.content,
+            csvMapping
+        )
 
         if (transactionsGroup == undefined) {
-            throw new Error('Unable to convert CSV file to transactions group');
+            throw new Error('Unable to convert CSV file to transactions group')
         }
 
         return {
@@ -159,52 +170,57 @@ export class BankAccountLoader implements IBankAccountLoader {
             accountId: account.accountId,
             accountType: account.accountType,
             transactionsGroups: [transactionsGroup]
-        } as BankAccount;
+        } as BankAccount
 
         // return await this.csvToBankAccount.loadCsvFile(file);
     }
 
     public sanitize(accounts: BankAccountsDictionary) {
-        
-        const newAccounts = this.combineAndSortTransactionsGroups();
+        const newAccounts = this.combineAndSortTransactionsGroups()
 
-        this.sanitizedAccountsById = this.sanitizeNewAccounts(accounts, newAccounts);
+        this.sanitizedAccountsById = this.sanitizeNewAccounts(accounts, newAccounts)
     }
 
-    public combineAndSortTransactionsGroups() : BankAccountsDictionary {
-        const accountsById : BankAccountsDictionary = {};
+    public combineAndSortTransactionsGroups(): BankAccountsDictionary {
+        const accountsById: BankAccountsDictionary = {}
 
         for (const accountId in this.rawAccountsLoadedById) {
-            const loadedAccounts = this.rawAccountsLoadedById[accountId];
-            const combinedGroups = this.bankAccountOperations.getCombinedTransactionsGroup(...loadedAccounts);
-            const sortedGroups = this.bankAccountOperations.sortTransactionsGroupByStartDateAscending(combinedGroups);
-            const account = loadedAccounts[0];
+            const loadedAccounts = this.rawAccountsLoadedById[accountId]
+            const combinedGroups = this.bankAccountOperations.getCombinedTransactionsGroup(
+                ...loadedAccounts
+            )
+            const sortedGroups =
+                this.bankAccountOperations.sortTransactionsGroupByStartDateAscending(combinedGroups)
+            const account = loadedAccounts[0]
             accountsById[account.accountId] = {
                 ...account,
                 transactionsGroups: sortedGroups
-            };
+            }
         }
 
-        return accountsById;
+        return accountsById
     }
 
-    public sanitizeNewAccounts(existingAccounts: BankAccountsDictionary, newAccounts: BankAccountsDictionary) : BankAccountsDictionary {
-
-        const sanitizedAccountsById : BankAccountsDictionary = {};
+    public sanitizeNewAccounts(
+        existingAccounts: BankAccountsDictionary,
+        newAccounts: BankAccountsDictionary
+    ): BankAccountsDictionary {
+        const sanitizedAccountsById: BankAccountsDictionary = {}
 
         for (const id in newAccounts) {
-            const sanitizer = this.bankAccountTransactionsSanitizerFactory.create(existingAccounts[id]);
+            const sanitizer = this.bankAccountTransactionsSanitizerFactory.create(
+                existingAccounts[id]
+            )
 
-            const account = newAccounts[id];
+            const account = newAccounts[id]
             for (const group of account.transactionsGroups) {
-                sanitizer.addTransactionsGroup(group);
+                sanitizer.addTransactionsGroup(group)
             }
-            account.transactionsGroups = sanitizer.transactionsGroups;
+            account.transactionsGroups = sanitizer.transactionsGroups
 
-            sanitizedAccountsById[id] = account;
+            sanitizedAccountsById[id] = account
         }
 
-        return sanitizedAccountsById;
+        return sanitizedAccountsById
     }
 }
-
--- src/views/account/BdgAddStatement.vue
+++ src/views/account/BdgAddStatement.vue
@@ -1,144 +1,161 @@
 <template>
-  <div>
-    <account-header :bank-account="targetAccount">
-      <v-btn
-        flat
-        :to="{path: 'transactions', replace: true}">
-<!--        <v-icon-->
-<!--          size="24"-->
-<!--          icon="mdi-file-upload-outline"></v-icon>-->
-        transactions
-      </v-btn>
-    </account-header>
-    <div class="page-container pa-2">
-      <div class="d-flex flex-column align-content-start h-100">
-        <v-card class="file-input-card pt-4 pr-4 pl-4 pb-4 mr-2 mb-2" :class="{'d-none': statementPresent}">
-          <div class="d-flex flex-row">
-            <div class="d-flex flex-column justify-center mr-4 mb-4">
-              <label for="ofx-file-input">
-                OFX file
-              </label>
+    <div>
+        <account-header :bank-account="targetAccount">
+            <v-btn flat :to="{ path: 'transactions', replace: true }">
+                <!--        <v-icon-->
+                <!--          size="24"-->
+                <!--          icon="mdi-file-upload-outline"></v-icon>-->
+                transactions
+            </v-btn>
+        </account-header>
+        <div class="page-container pa-2">
+            <div class="d-flex flex-column align-content-start h-100">
+                <v-card
+                    class="file-input-card pt-4 pr-4 pl-4 pb-4 mr-2 mb-2"
+                    :class="{ 'd-none': statementPresent }"
+                >
+                    <div class="d-flex flex-row">
+                        <div class="d-flex flex-column justify-center mr-4 mb-4">
+                            <label for="ofx-file-input"> OFX file </label>
+                        </div>
+                        <v-file-input
+                            id="ofx-file-input"
+                            class=""
+                            v-model="ofxFileName"
+                            :disabled="addStatementStore.loading"
+                            @update:modelValue="onFileNameUpdated"
+                            accept=".ofx,.csv"
+                            :multiple="true"
+                        ></v-file-input>
+                    </div>
+                </v-card>
+                <v-card class="action-card" v-show="statementPresent">
+                    <div class="d-flex flex-column align-content-center ma-5">
+                        <v-expansion-panels class="elevation-0">
+                            <bdg-account-added
+                                v-for="id in accountsIds"
+                                :key="id"
+                                :accountId="id"
+                            ></bdg-account-added>
+                        </v-expansion-panels>
+                    </div>
+                    <v-card-actions class="d-flex flex-grow-1 flex-row justify-center">
+                        <v-btn @click="onAdd">Add</v-btn>
+                        <v-btn @click="onDiscard()">Discard</v-btn>
+                    </v-card-actions>
+                </v-card>
             </div>
-            <v-file-input
-              id="ofx-file-input"
-              class=""
-              v-model="ofxFileName"
-              :disabled = "addStatementStore.loading"
-              @update:modelValue="onFileNameUpdated"
-              accept=".ofx,.csv"
-              :multiple="true"
-            ></v-file-input>
-          </div>
-        </v-card>
-        <v-card class="action-card" v-show="statementPresent">
-          <div class="d-flex flex-column align-content-center ma-5">
-            <v-expansion-panels class="elevation-0">
-              <bdg-account-added v-for="id in accountsIds" :key="id" :accountId="id"></bdg-account-added>
-            </v-expansion-panels>
-          </div>
-          <v-card-actions class="d-flex flex-grow-1 flex-row justify-center">
-            <v-btn @click="onAdd">Add</v-btn>
-            <v-btn @click="onDiscard()">Discard</v-btn>
-          </v-card-actions>
-        </v-card>
-      </div>
+        </div>
     </div>
-
-  </div>
 </template>
 
 <style scoped>
-  .page-container {
-    height: 100%;
-  }
+    .page-container {
+        height: 100%;
+    }
 
-  .file-input-card {
-    flex: 1 1 0;
-    display: block;
-    position: relative;
-    min-height: 10em;
-  }
+    .file-input-card {
+        flex: 1 1 0;
+        display: block;
+        position: relative;
+        min-height: 10em;
+    }
 </style>
 
 <script setup lang="ts">
-  import AccountHeader from '@views/account/BdgAccountHeader.vue'
-  import BdgAccountAdded from '@views/account/BdgAccountAdded.vue'
-  import { computed, defineModel } from 'vue'
-  import { useAddStatementStore } from '@/stores/add-statement-store'
-  import { useBankAccountsStore } from '@/stores/bankAccounts-store'
-  import { container } from '@/core/setupInversify'
-  import type { IBankAccountLoader } from '@services/BankAccountLoader'
-  import { ServicesTypes } from '@services/types'
-  import { useRoute } from 'vue-router'
+    import AccountHeader from '@views/account/BdgAccountHeader.vue'
+    import BdgAccountAdded from '@views/account/BdgAccountAdded.vue'
+    import { computed, defineModel } from 'vue'
+    import { useAddStatementStore } from '@/stores/add-statement-store'
+    import { useBankAccountsStore } from '@/stores/bankAccounts-store'
+    import { container } from '@/core/setupInversify'
+    import type { IBankAccountLoader } from '@services/BankAccountLoader'
+    import { ServicesTypes } from '@services/types'
+    import { useRoute } from 'vue-router'
+    import { useCsvSettingsStore } from '@/stores/csvSettings-store'
 
-  const ofxFileName = defineModel<File[]>();
-  const addStatementStore = useAddStatementStore();
-  const bankAccountStore = useBankAccountsStore();
-  const route = useRoute();
+    const ofxFileName = defineModel<File[]>()
+    const addStatementStore = useAddStatementStore()
+    const bankAccountStore = useBankAccountsStore()
+    const csvSettingsStore = useCsvSettingsStore()
+    const route = useRoute()
 
-  const targetAccountId = computed(() => {
-    return route.params.id as string;
-  });
+    const targetAccountId = computed(() => {
+        return route.params.id as string
+    })
 
-  const accountsIds = computed(() => {
-    return addStatementStore.accountsIds;
-  });
+    const accountsIds = computed(() => {
+        return addStatementStore.accountsIds
+    })
 
-  const targetAccount = computed(() => {
-    return bankAccountStore.getAccountById(targetAccountId.value);
-  });
+    const targetAccount = computed(() => {
+        return bankAccountStore.getAccountById(targetAccountId.value)
+    })
 
+    const statementPresent = computed(() => {
+        return accountsIds.value.length > 0
+    })
 
-  const statementPresent = computed(() => {
-    return accountsIds.value.length > 0;
-  });
+    async function onFileNameUpdated(files: File[] | File) {
+        if (!targetAccount.value.csvSettingId) {
+            console.error('No CSV setting ID found for the target account')
+            return
+        }
 
+        const csvSettings = csvSettingsStore.getSettings(targetAccount.value.csvSettingId)
 
-  async function onFileNameUpdated(files: File[] | File) {
+        if (!csvSettings) {
+            console.error('No CSV settings found for the target account')
+            return
+        }
 
-    const filesArray = Array.isArray(files)? files : [files];
+        const filesArray = Array.isArray(files) ? files : [files]
 
-    if (filesArray.length == 0) {
-      return;
+        if (filesArray.length == 0) {
+            return
+        }
+
+        const bankAccountLoader = container.get<IBankAccountLoader>(ServicesTypes.BankAccountLoader)
+
+        if (!bankAccountLoader) {
+            throw new Error('No BankAccountLoader service found')
+        }
+
+        bankAccountLoader.loadingFileStarted = (fileName: string) => {
+            addStatementStore.setLoadingFile(fileName)
+        }
+
+        await bankAccountLoader.loadWithAccount(
+            targetAccount.value,
+            csvSettings.columnsMapping,
+            filesArray
+        )
+        bankAccountLoader.sanitize(bankAccountStore.accounts)
+
+        for (const id in bankAccountLoader.accountsById) {
+            const account = bankAccountLoader.accountsById[id]
+            addStatementStore.setBankAccount(account)
+        }
+
+        addStatementStore.clearLoadingFileStatus()
     }
 
-    const bankAccountLoader = container.get<IBankAccountLoader>(ServicesTypes.BankAccountLoader);
-
-    if (!bankAccountLoader) {
-      throw new Error('No BankAccountLoader service found');
+    function clear() {
+        ofxFileName.value = []
+        addStatementStore.clear()
     }
 
-    bankAccountLoader.loadingFileStarted = (fileName: string) => {
-      addStatementStore.setLoadingFile(fileName);
+    function onDiscard() {
+        clear()
     }
 
-    await bankAccountLoader.loadWithAccount(targetAccount.value, filesArray );
-    bankAccountLoader.sanitize(bankAccountStore.accounts);
+    function onAdd() {
+        const accounts = addStatementStore.accounts
 
-    for (const id in bankAccountLoader.accountsById) {
-      const account = bankAccountLoader.accountsById[id];
-      addStatementStore.setBankAccount(account);
+        Object.values(accounts).forEach((account) => {
+            bankAccountStore.addWithBankAccount(account, targetAccountId.value)
+        })
+
+        clear()
     }
-
-    addStatementStore.clearLoadingFileStatus();
-  }
-
-  function clear() {
-    ofxFileName.value = [];
-    addStatementStore.clear();
-  }
-
-  function onDiscard() {
-    clear();
-  }
-
-  function onAdd() {
-    const accounts = addStatementStore.accounts;
-
-    Object.values(accounts).forEach((account) => {
-      bankAccountStore.addWithBankAccount(account, targetAccountId.value);
-    });
-
-    clear();
-  }
 </script>
