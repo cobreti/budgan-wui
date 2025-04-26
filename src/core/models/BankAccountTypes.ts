62538f4b1ae237fbf659a35dc0bf1d067488f654
Danny Thibaudeau <cobreti_@hotmail.com>
2025-04-26 17:40:27 -0400

    Features/add csvsettings to bankaccount
    * added csv settings dropdown
    
    * added delete account button

--- src/core/models/BankAccountTypes.ts
+++ src/core/models/BankAccountTypes.ts
@@ -1,48 +1,48 @@
 /**
  * Defines transaction Ids table.
- * 
+ *
  * Using key to store ids and an empty object assigned to this id
  * so it can be saved and restored automatically from browser session storage.
- * 
+ *
  */
-export declare type TransactionIdsTable = {[key: string]: object};
+export declare type TransactionIdsTable = { [key: string]: object }
 
 export type BankAccountTransaction = {
-    transactionId: string;
-    dateInscription: Date;
-    dateTransaction?: Date;
-    amount: number;
-    type: string;
-    description: string;
+    transactionId: string
+    dateInscription: Date
+    dateTransaction?: Date
+    amount: number
+    type: string
+    description: string
 }
 
 export enum InvalidTransactionReason {
     // eslint-disable-next-line no-unused-vars
     unknown = 'unknown',
     // eslint-disable-next-line no-unused-vars
-    duplicate = 'duplicate',
+    duplicate = 'duplicate'
 }
 
 export type BankAccountInvalidTransaction = BankAccountTransaction & {
-    invalidReason: InvalidTransactionReason;
+    invalidReason: InvalidTransactionReason
 }
 
 export type BankAccountTransactionsGroup = {
-    name: string;
-    filename?: string;
-    id: string;
-    dateStart: Date;
-    dateEnd: Date;
-    transactions: BankAccountTransaction[];
-    invalidTransactions?: BankAccountInvalidTransaction[];
+    name: string
+    filename?: string
+    id: string
+    dateStart: Date
+    dateEnd: Date
+    transactions: BankAccountTransaction[]
+    invalidTransactions?: BankAccountInvalidTransaction[]
 }
 
 export type BankAccount = {
-    name: string;
-    accountId: string;
-    accountType: string | undefined;
-    transactionsGroups: BankAccountTransactionsGroup[];
+    name: string
+    accountId: string
+    accountType: string | undefined
+    transactionsGroups: BankAccountTransactionsGroup[]
+    csvSettingId?: string
 }
 
-export type BankAccountsDictionary = {[key: string]: BankAccount};
-
+export type BankAccountsDictionary = { [key: string]: BankAccount }
--- src/views/accountsManagement/BdgNewAccount.vue
+++ src/views/accountsManagement/BdgNewAccount.vue
@@ -1,112 +1,133 @@
 <template>
-  <div>
-    <v-toolbar class="pl-5">
-      <a @click="$router.go(-1)">
-        <v-icon
-          size="Large"
-          icon="mdi-chevron-left"></v-icon>
-      </a>
-    </v-toolbar>
-    <div class="ma-5 form">
-      <v-form fast-fail @submit.prevent v-model="formValid">
-        <v-text-field
-          label="Account Name"
-          required
-          :rules = "accountNameRules"
-          v-model="accountName"></v-text-field>
-        <v-text-field
-          label="Account Number"
-          required
-          :rules = "accountNumberRules"
-          v-model="accountNumber"></v-text-field>
-        <v-select
-          required
-          label="Account Type"
-          :items="accountTypes"
-          :rules = "accountTypeRule"
-          v-model="accountType"></v-select>
-        <v-btn
-          type="submit"
-          @click="createAccount"
-          color="primary"
-          :disabled="!formValid"
-          block>Create Account</v-btn>
-      </v-form>
+    <div>
+        <v-toolbar class="pl-5">
+            <a @click="$router.go(-1)">
+                <v-icon size="Large" icon="mdi-chevron-left"></v-icon>
+            </a>
+        </v-toolbar>
+        <div class="ma-5 form">
+            <v-form fast-fail @submit.prevent v-model="formValid">
+                <v-text-field
+                    label="Account Name"
+                    required
+                    :rules="accountNameRules"
+                    v-model="accountName"
+                ></v-text-field>
+                <v-text-field
+                    label="Account Number"
+                    required
+                    :rules="accountNumberRules"
+                    v-model="accountNumber"
+                ></v-text-field>
+                <v-select
+                    required
+                    label="Account Type"
+                    :items="accountTypes"
+                    :rules="accountTypeRule"
+                    v-model="accountType"
+                ></v-select>
+                <v-select
+                    required
+                    label="CSV Setting"
+                    :items="csvSettings"
+                    v-model="selectedCsvSetting"
+                    :item-value="'id'"
+                    :item-title="'name'"
+                    :rules="csvSettingRules"
+                ></v-select>
+                <v-btn
+                    type="submit"
+                    @click="createAccount"
+                    color="primary"
+                    :disabled="!formValid"
+                    block
+                    >Create Account</v-btn
+                >
+            </v-form>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
-  a {
-    cursor: pointer;
-  }
+    a {
+        cursor: pointer;
+    }
 
-  .form {
-    max-width: 40em;
-  }
+    .form {
+        max-width: 40em;
+    }
 </style>
 
 <script setup lang="ts">
+    import { useBankAccountsStore } from '@/stores/bankAccounts-store'
+    import type { BankAccount } from '@models/BankAccountTypes'
+    import router from '@/router'
+    import { computed } from 'vue'
+    import { useCsvSettingsStore } from '@/stores/csvSettings-store'
 
-  import { useBankAccountsStore } from '@/stores/bankAccounts-store'
-  import type { BankAccount } from '@models/BankAccountTypes'
-  import router from '@/router'
-  import { computed } from 'vue'
+    const bankAccountStore = useBankAccountsStore()
+    const csvSettingsStore = useCsvSettingsStore()
 
-  const bankAccountStore = useBankAccountsStore();
+    const accountName = defineModel<string>('accountName', { required: true })
+    const accountNumber = defineModel<string>('accountNumber', { required: true })
+    const accountType = defineModel<string>('accountType', { required: true })
+    const formValid = defineModel<boolean>('formValid')
+    const accountTypes = ['Checking', 'Savings', 'Credit Card']
+    const csvSettings = computed(() => csvSettingsStore.settings)
+    const selectedCsvSetting = defineModel<string>('selectedCsvSetting', { required: true })
 
-  const accountName = defineModel<string>('accountName', {required: true});
-  const accountNumber = defineModel<string>('accountNumber', {required: true});
-  const accountType = defineModel<string>('accountType', {required: true});
-  const formValid = defineModel<boolean>('formValid');
-  const accountTypes = ['Checking', 'Savings', 'Credit Card'];
+    const accountNames = computed(() => {
+        return Object.values(bankAccountStore.accounts).map((account) => account.name)
+    })
 
-  const accountNames = computed(() => {
-    return Object.values(bankAccountStore.accounts).map(account => account.name);
-  })
+    const accountNameRules = [
+        (v: string) => {
+            if (!v) return 'Account name is required'
 
-  const accountNameRules = [
-    (v: string) => {
-      if (!v) return 'Account name is required';
+            if (accountNames.value.includes(v)) return 'Account name already exists'
 
-      if (accountNames.value.includes(v)) return 'Account name already exists';
+            return true
+        }
+    ]
 
-      return true;
-    },
-  ];
+    const accountNumberRules = [
+        (v: string) => {
+            if (!v) return 'Account number is required'
 
-  const accountNumberRules = [
-    (v: string) => {
+            const account = bankAccountStore.getAccountByIdIfExist(v)
+            if (account) return 'Account number already exists'
 
-      if (!v) return 'Account number is required';
+            return true
+        }
+    ]
 
-      const account = bankAccountStore.getAccountByIdIfExist(v);
-      if (account) return 'Account number already exists';
+    const accountTypeRule = [
+        (v: string) => {
+            if (!v) return 'Account type is required'
 
-      return true;
-    },
-  ]
+            return true
+        }
+    ]
 
-  const accountTypeRule = [
-    (v: string) => {
-      if (!v) return 'Account type is required';
+    const csvSettingRules = [
+        (v: string) => {
+            if (!v) return 'CSV setting is required'
 
-      return true;
+            return true
+        }
+    ]
+
+    async function createAccount() {
+        const bankAccount: BankAccount = {
+            name: accountName.value,
+            accountId: accountNumber.value,
+            accountType: accountType.value,
+            transactionsGroups: [],
+            csvSettingId: selectedCsvSetting.value
+        }
+
+        bankAccountStore.addWithBankAccount(bankAccount)
+
+        router.go(-1)
     }
-  ]
-
-  async function createAccount() {
-    const bankAccount : BankAccount = {
-      name: accountName.value,
-      accountId: accountNumber.value,
-      accountType: accountType.value,
-      transactionsGroups: []
-    };
-
-    bankAccountStore.addWithBankAccount(bankAccount);
-
-    router.go(-1);
-  }
-
 </script>
-
