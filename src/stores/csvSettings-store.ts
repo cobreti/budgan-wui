0eb17f82361ae3a9a715d7d8b26b32fcbc2cb25b
Danny Thibaudeau <cobreti_@hotmail.com>
2025-04-26 21:01:00 -0400

    Features/use csvsettings on load
    * allow null column type
    
    * use column mapping from csv settings when loading
    
    * removed commented lines

--- src/stores/csvSettings-store.ts
+++ src/stores/csvSettings-store.ts
@@ -1,31 +1,41 @@
-import type { CSVSettings, CSVSettingsList } from "@/core/models/csvDocument"
-import { defineStore } from "pinia";
-import { ref, type Ref } from "vue";
+import type { CSVSettings, CSVSettingsList } from '@/core/models/csvDocument'
+import { defineStore } from 'pinia'
+import { ref, type Ref } from 'vue'
 
 export type CsvSettingsStore = {
-    settings: Ref<CSVSettingsList>,
-    addSetting: (setting: CSVSettings) => void,
-    removeSetting: (id: string) => void,
+    settings: Ref<CSVSettingsList>
+    addSetting: (setting: CSVSettings) => void
+    removeSetting: (id: string) => void
+    getSettings: (id: string) => CSVSettings | undefined
 }
 
-export const useCsvSettingsStore = defineStore<string, CsvSettingsStore>('csvSettings', () => {
-    const settings = ref<CSVSettingsList>([]);
+export const useCsvSettingsStore = defineStore<string, CsvSettingsStore>(
+    'csvSettings',
+    () => {
+        const settings = ref<CSVSettingsList>([])
 
-    function addSetting(setting: CSVSettings) {
-        settings.value.push(setting);
-    }
+        function addSetting(setting: CSVSettings) {
+            settings.value.push(setting)
+        }
 
-    function removeSetting(id: string) {
-        settings.value = settings.value.filter((setting) => setting.id !== id);
-    }
+        function removeSetting(id: string) {
+            settings.value = settings.value.filter((setting) => setting.id !== id)
+        }
 
-    return {
-        settings,
-        addSetting,
-        removeSetting
-    };
-},{
-    persist: {
-      storage: localStorage
+        function getSettings(id: string): CSVSettings | undefined {
+            return settings.value.find((setting) => setting.id === id)
+        }
+
+        return {
+            settings,
+            addSetting,
+            removeSetting,
+            getSettings
+        }
     },
-  });
+    {
+        persist: {
+            storage: localStorage
+        }
+    }
+)
