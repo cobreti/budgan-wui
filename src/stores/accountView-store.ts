d65255112f95d744954afb885be5a2d0b6962125
Danny T <cobreti_@hotmail.com>
2025-05-11 15:27:34 -0400

    simplified account view

--- src/stores/accountView-store.ts
+++ src/stores/accountView-store.ts
@@ -1,58 +1,76 @@
-import type {BankAccount, BankAccountTransaction} from '@models/BankAccountTypes';
-import {defineStore} from 'pinia';
-import {type Ref, ref} from 'vue';
+import type { BankAccount, BankAccountTransaction } from '@models/BankAccountTypes'
+import { defineStore } from 'pinia'
+import { type Ref, ref } from 'vue'
+import { useBankAccountsStore } from '@/stores/bankAccounts-store'
 
 export type AccountViewTransaction = BankAccountTransaction & {
-    groupId: string;
+    groupId: string
 }
 
 export type AccountView = {
-    account: BankAccount | undefined;
-    transactions: AccountViewTransaction[];
+    account: BankAccount | undefined
+    transactions: AccountViewTransaction[]
 }
 
 export type AccountViewStore = {
-    accountView: Ref<AccountView>;
-    addBankAccount: (bankAccount: BankAccount) => void;
-    clearAccountView: () => void;
-};
+    accountView: Ref<AccountView>
+    setBankAccountFromId: (id: string) => void
+    addBankAccount: (bankAccount: BankAccount) => void
+    clearAccountView: () => void
+}
 
-
-export const useAccountViewStore = defineStore<string, AccountViewStore>('accountView',  () => {
+export const useAccountViewStore = defineStore<string, AccountViewStore>('accountView', () => {
+    const bankAccountStore = useBankAccountsStore()
 
     const accountView = ref<AccountView>({
         account: undefined,
         transactions: []
-    });
+    })
+
+    function setBankAccountFromId(id: string) {
+        const bankAccount = bankAccountStore.getAccountByIdIfExist(id)
+
+        clearAccountView()
+
+        if (bankAccount) {
+            addBankAccount(bankAccount)
+        }
+    }
 
     function addBankAccount(bankAccount: BankAccount) {
+        const transactions = bankAccount.transactionsGroups.reduce(
+            (acc: AccountViewTransaction[], group) => {
+                const groupId = group.id
 
-        const transactions = bankAccount.transactionsGroups.reduce((acc: AccountViewTransaction[], group) => {
-            const groupId = group.id;
+                const viewTransactions = group.transactions.reduce(
+                    (tacc: AccountViewTransaction[], transaction) => {
+                        return [...tacc, { ...transaction, groupId }]
+                    },
+                    []
+                )
 
-            const viewTransactions = group.transactions.reduce((tacc: AccountViewTransaction[], transaction) => {
-                return [...tacc, {...transaction, groupId}];
-            }, []);
-
-            return [...acc, ...viewTransactions];
-        }, []);
+                return [...acc, ...viewTransactions]
+            },
+            []
+        )
 
         accountView.value = {
             account: bankAccount,
             transactions
-        };
+        }
     }
 
     function clearAccountView() {
         accountView.value = {
             account: undefined,
             transactions: []
-        };
+        }
     }
 
     return {
         accountView,
+        setBankAccountFromId,
         addBankAccount,
         clearAccountView
-    };
-});
+    }
+})
