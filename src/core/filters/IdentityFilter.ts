4352d4e97c01acf7bc1880a67f65d8f9039a38e8
Danny T <cobreti_@hotmail.com>
2025-05-19 21:51:10 -0400

    fixed to not use transactionsGroups

--- src/core/filters/IdentityFilter.ts
+++ src/core/filters/IdentityFilter.ts
@@ -1,36 +1,46 @@
 import type { BankAccount, TransactionIdsTable } from '@models/BankAccountTypes'
 import type { FilteredTransactions, TransactionsFilterFct } from '@models/FilterTypes'
 
-export const IdentityFilter : TransactionsFilterFct = (account: BankAccount | undefined) : FilteredTransactions => {
-  if (!account) {
-    return {
-      dateStart: undefined,
-      dateEnd: undefined,
-      transactions: [],
-      transactionsIds: {}
+export const IdentityFilter: TransactionsFilterFct = (
+    account: BankAccount | undefined
+): FilteredTransactions => {
+    if (!account || !account.transactions || account.transactions.length === 0) {
+        // If no transactions, return empty object
+        return {
+            dateStart: undefined,
+            dateEnd: undefined,
+            transactions: [],
+            transactionsIds: {}
+        }
     }
-  }
 
-  let minDate: Date | undefined = undefined;
-  let maxDate: Date | undefined = undefined;
-  const transactionsIds : TransactionIdsTable = {};
-  const transactions = account.transactionsGroups.flatMap(group => {
-    minDate = minDate ? (group.dateStart < minDate ? group.dateStart : minDate) : group.dateStart;
-    maxDate = maxDate ? (group.dateEnd > maxDate ? group.dateEnd : maxDate) : group.dateEnd;
-    return group.transactions.map(transaction => {
-      transactionsIds[transaction.transactionId] = {};
-      return {
-        ...transaction,
-        bankAccountId: account.accountId,
-        transactionGroupId: group.id
-      }
-    });
-  });
+    const initialDate = account.transactions[0].dateInscription
 
-  return {
-    dateStart: minDate,
-    dateEnd: maxDate,
-    transactions,
-    transactionsIds
-  }
+    const minDate = account.transactions.reduce((min, transaction) => {
+        const transactionDate = transaction.dateInscription
+        if (transactionDate < min) {
+            return transactionDate
+        }
+        return min
+    }, initialDate)
+
+    const maxDate = account.transactions.reduce((max, transaction) => {
+        const transactionDate = transaction.dateInscription
+        if (transactionDate > max) {
+            return transactionDate
+        }
+        return max
+    }, initialDate)
+
+    const transactionsIds = account.transactions.reduce((acc, transaction) => {
+        acc[transaction.transactionId] = {}
+        return acc
+    }, {} as TransactionIdsTable)
+
+    return {
+        dateStart: minDate,
+        dateEnd: maxDate,
+        transactions: account.transactions,
+        transactionsIds
+    }
 }
