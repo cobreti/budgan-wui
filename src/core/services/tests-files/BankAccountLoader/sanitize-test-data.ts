3fe80714feefaabd98239fdcf4d71f4100858816
Danny T <cobreti_@hotmail.com>
2025-05-19 20:52:08 -0400

    fixed data.

--- src/core/services/tests-files/BankAccountLoader/sanitize-test-data.ts
+++ src/core/services/tests-files/BankAccountLoader/sanitize-test-data.ts
@@ -5,23 +5,22 @@ export const sanitizeNewAccounts_existingAccount: BankAccountsDictionary = {
         name: 'test',
         accountId: '123456789',
         accountType: 'checking',
-        transactions: [],
+        transactions: [
+            {
+                transactionId: '1',
+                transactionGroupId: '1',
+                dateInscription: new Date(),
+                amount: 1,
+                type: 'debit',
+                description: 'test'
+            }
+        ],
         transactionsGroups: [
             {
                 name: 'test',
                 id: '1',
                 dateStart: new Date('2023-01-01'),
-                dateEnd: new Date('2023-01-31'),
-                transactions: [
-                    {
-                        transactionId: '1',
-                        transactionGroupId: '1',
-                        dateInscription: new Date(),
-                        amount: 1,
-                        type: 'debit',
-                        description: 'test'
-                    }
-                ]
+                dateEnd: new Date('2023-01-31')
             }
         ]
     }
@@ -32,39 +31,36 @@ export const sanitizeNewAccounts_newAccounts: BankAccountsDictionary = {
         name: 'Account One',
         accountId: '987654321',
         accountType: 'savings',
-        transactions: [],
+        transactions: [
+            {
+                transactionId: 'trans1-1',
+                transactionGroupId: 'group1-1',
+                dateInscription: new Date('2023-01-10'),
+                amount: 100,
+                type: 'DEPOSIT',
+                description: 'Initial Deposit'
+            },
+            {
+                transactionId: 'trans1-2',
+                transactionGroupId: 'group1-2',
+                dateInscription: new Date('2023-02-15'),
+                amount: 200,
+                type: 'DEPOSIT',
+                description: 'Monthly Deposit'
+            }
+        ],
         transactionsGroups: [
             {
                 name: 'Group 1',
                 id: 'group1-1',
                 dateStart: new Date('2023-01-01'),
-                dateEnd: new Date('2023-01-31'),
-                transactions: [
-                    {
-                        transactionId: 'trans1-1',
-                        transactionGroupId: 'group1-1',
-                        dateInscription: new Date('2023-01-10'),
-                        amount: 100,
-                        type: 'DEPOSIT',
-                        description: 'Initial Deposit'
-                    }
-                ]
+                dateEnd: new Date('2023-01-31')
             },
             {
                 name: 'Group 2',
                 id: 'group1-2',
                 dateStart: new Date('2023-02-01'),
-                dateEnd: new Date('2023-02-28'),
-                transactions: [
-                    {
-                        transactionId: 'trans1-2',
-                        transactionGroupId: 'group1-2',
-                        dateInscription: new Date('2023-02-15'),
-                        amount: 200,
-                        type: 'DEPOSIT',
-                        description: 'Monthly Deposit'
-                    }
-                ]
+                dateEnd: new Date('2023-02-28')
             }
         ]
     },
@@ -72,39 +68,36 @@ export const sanitizeNewAccounts_newAccounts: BankAccountsDictionary = {
         name: 'test',
         accountId: '123456789',
         accountType: 'checking',
-        transactions: [],
+        transactions: [
+            {
+                transactionId: 'trans2-1',
+                transactionGroupId: 'group2-1',
+                dateInscription: new Date('2023-03-10'),
+                amount: -50,
+                type: 'WITHDRAWAL',
+                description: 'Utility Bill'
+            },
+            {
+                transactionId: 'trans2-2',
+                transactionGroupId: 'group2-2',
+                dateInscription: new Date('2023-04-15'),
+                amount: 150,
+                type: 'DEPOSIT',
+                description: 'Paycheck'
+            }
+        ],
         transactionsGroups: [
             {
                 name: 'Group 1',
                 id: 'group2-1',
                 dateStart: new Date('2023-03-01'),
-                dateEnd: new Date('2023-03-31'),
-                transactions: [
-                    {
-                        transactionId: 'trans2-1',
-                        transactionGroupId: 'group2-1',
-                        dateInscription: new Date('2023-03-10'),
-                        amount: -50,
-                        type: 'WITHDRAWAL',
-                        description: 'Utility Bill'
-                    }
-                ]
+                dateEnd: new Date('2023-03-31')
             },
             {
                 name: 'Group 2',
                 id: 'group2-2',
                 dateStart: new Date('2023-04-01'),
-                dateEnd: new Date('2023-04-30'),
-                transactions: [
-                    {
-                        transactionId: 'trans2-2',
-                        transactionGroupId: 'group2-2',
-                        dateInscription: new Date('2023-04-15'),
-                        amount: 150,
-                        type: 'DEPOSIT',
-                        description: 'Paycheck'
-                    }
-                ]
+                dateEnd: new Date('2023-04-30')
             }
         ]
     }
@@ -116,49 +109,19 @@ export const sanitizeNewAccounts_transactionsGroups_account_123456789: BankAccou
             name: 'test',
             id: '1',
             dateStart: new Date('2023-01-01'),
-            dateEnd: new Date('2023-01-31'),
-            transactions: [
-                {
-                    transactionId: '1',
-                    transactionGroupId: '1',
-                    dateInscription: new Date(),
-                    amount: 1,
-                    type: 'debit',
-                    description: 'test'
-                }
-            ]
+            dateEnd: new Date('2023-01-31')
         },
         {
             name: 'Group 1',
             id: 'group2-1',
             dateStart: new Date('2023-03-01'),
-            dateEnd: new Date('2023-03-31'),
-            transactions: [
-                {
-                    transactionId: 'trans2-1',
-                    transactionGroupId: 'group2-1',
-                    dateInscription: new Date('2023-03-10'),
-                    amount: -50,
-                    type: 'WITHDRAWAL',
-                    description: 'Utility Bill'
-                }
-            ]
+            dateEnd: new Date('2023-03-31')
         },
         {
             name: 'Group 2',
             id: 'group2-2',
             dateStart: new Date('2023-04-01'),
-            dateEnd: new Date('2023-04-30'),
-            transactions: [
-                {
-                    transactionId: 'trans2-2',
-                    transactionGroupId: 'group2-2',
-                    dateInscription: new Date('2023-04-15'),
-                    amount: 150,
-                    type: 'DEPOSIT',
-                    description: 'Paycheck'
-                }
-            ]
+            dateEnd: new Date('2023-04-30')
         }
     ]
 
@@ -168,33 +131,13 @@ export const sanitizeNewAccounts_transactionsGroups_account_987654321: BankAccou
             name: 'Group 1',
             id: 'group1-1',
             dateStart: new Date('2023-01-01'),
-            dateEnd: new Date('2023-01-31'),
-            transactions: [
-                {
-                    transactionId: 'trans1-1',
-                    transactionGroupId: 'group1-1',
-                    dateInscription: new Date('2023-01-10'),
-                    amount: 100,
-                    type: 'DEPOSIT',
-                    description: 'Initial Deposit'
-                }
-            ]
+            dateEnd: new Date('2023-01-31')
         },
         {
             name: 'Group 2',
             id: 'group1-2',
             dateStart: new Date('2023-02-01'),
-            dateEnd: new Date('2023-02-28'),
-            transactions: [
-                {
-                    transactionId: 'trans1-2',
-                    transactionGroupId: 'group1-2',
-                    dateInscription: new Date('2023-02-15'),
-                    amount: 200,
-                    type: 'DEPOSIT',
-                    description: 'Monthly Deposit'
-                }
-            ]
+            dateEnd: new Date('2023-02-28')
         }
     ]
 
@@ -203,39 +146,36 @@ export const sanitizeNewAccounts_accountsById_expected_data: BankAccountsDiction
         name: 'Account One',
         accountId: '987654321',
         accountType: 'savings',
-        transactions: [],
+        transactions: [
+            {
+                transactionId: 'trans1-1',
+                transactionGroupId: 'group1-1',
+                dateInscription: new Date('2023-01-10'),
+                amount: 100,
+                type: 'DEPOSIT',
+                description: 'Initial Deposit'
+            },
+            {
+                transactionId: 'trans1-2',
+                transactionGroupId: 'group1-2',
+                dateInscription: new Date('2023-02-15'),
+                amount: 200,
+                type: 'DEPOSIT',
+                description: 'Monthly Deposit'
+            }
+        ],
         transactionsGroups: [
             {
                 name: 'Group 1',
                 id: 'group1-1',
                 dateStart: new Date('2023-01-01'),
-                dateEnd: new Date('2023-01-31'),
-                transactions: [
-                    {
-                        transactionId: 'trans1-1',
-                        transactionGroupId: 'group1-1',
-                        dateInscription: new Date('2023-01-10'),
-                        amount: 100,
-                        type: 'DEPOSIT',
-                        description: 'Initial Deposit'
-                    }
-                ]
+                dateEnd: new Date('2023-01-31')
             },
             {
                 name: 'Group 2',
                 id: 'group1-2',
                 dateStart: new Date('2023-02-01'),
-                dateEnd: new Date('2023-02-28'),
-                transactions: [
-                    {
-                        transactionId: 'trans1-2',
-                        transactionGroupId: 'group1-2',
-                        dateInscription: new Date('2023-02-15'),
-                        amount: 200,
-                        type: 'DEPOSIT',
-                        description: 'Monthly Deposit'
-                    }
-                ]
+                dateEnd: new Date('2023-02-28')
             }
         ]
     },
@@ -243,55 +183,50 @@ export const sanitizeNewAccounts_accountsById_expected_data: BankAccountsDiction
         name: 'test',
         accountId: '123456789',
         accountType: 'checking',
-        transactions: [],
+        transactions: [
+            {
+                transactionId: '1',
+                transactionGroupId: '1',
+                dateInscription: new Date(),
+                amount: 1,
+                type: 'debit',
+                description: 'test'
+            },
+            {
+                transactionId: 'trans2-1',
+                transactionGroupId: 'group2-1',
+                dateInscription: new Date('2023-03-10'),
+                amount: -50,
+                type: 'WITHDRAWAL',
+                description: 'Utility Bill'
+            },
+            {
+                transactionId: 'trans2-2',
+                transactionGroupId: 'group2-2',
+                dateInscription: new Date('2023-04-15'),
+                amount: 150,
+                type: 'DEPOSIT',
+                description: 'Paycheck'
+            }
+        ],
         transactionsGroups: [
             {
                 name: 'test',
                 id: '1',
                 dateStart: new Date('2023-01-01'),
-                dateEnd: new Date('2023-01-31'),
-                transactions: [
-                    {
-                        transactionId: '1',
-                        transactionGroupId: '1',
-                        dateInscription: new Date(),
-                        amount: 1,
-                        type: 'debit',
-                        description: 'test'
-                    }
-                ]
+                dateEnd: new Date('2023-01-31')
             },
             {
                 name: 'Group 1',
                 id: 'group2-1',
                 dateStart: new Date('2023-03-01'),
-                dateEnd: new Date('2023-03-31'),
-                transactions: [
-                    {
-                        transactionId: 'trans2-1',
-                        transactionGroupId: 'group2-1',
-                        dateInscription: new Date('2023-03-10'),
-                        amount: -50,
-                        type: 'WITHDRAWAL',
-                        description: 'Utility Bill'
-                    }
-                ]
+                dateEnd: new Date('2023-03-31')
             },
             {
                 name: 'Group 2',
                 id: 'group2-2',
                 dateStart: new Date('2023-04-01'),
-                dateEnd: new Date('2023-04-30'),
-                transactions: [
-                    {
-                        transactionId: 'trans2-2',
-                        transactionGroupId: 'group2-2',
-                        dateInscription: new Date('2023-04-15'),
-                        amount: 150,
-                        type: 'DEPOSIT',
-                        description: 'Paycheck'
-                    }
-                ]
+                dateEnd: new Date('2023-04-30')
             }
         ]
     }
