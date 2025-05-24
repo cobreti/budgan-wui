93bbd70d97d238b37ebe8c46ed19c9bf18bdea4a
Danny Thibaudeau <cobreti_@hotmail.com>
2025-05-24 13:51:12 -0400

    added a way to go between account and csv settings quickly

--- src/views/settings/BdgSettingsPage.vue
+++ src/views/settings/BdgSettingsPage.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
-    <bdg-csv-settings></bdg-csv-settings>
-  </div>
+    <div>
+        <v-toolbar class="pl-5">
+            <a @click="navigateBack">
+                <v-icon size="Large" icon="mdi-chevron-left"></v-icon>
+            </a>
+            <v-toolbar-title class="ml-2">Settings</v-toolbar-title>
+        </v-toolbar>
+        <bdg-csv-settings></bdg-csv-settings>
+    </div>
 </template>
 
 <style scoped>
+    a {
+        cursor: pointer;
+    }
 </style>
 
 <script setup lang="ts">
+    import BdgCsvSettings from '@/views/settings/csvSettings/BdgCsvSettings.vue'
+    import { useRoute, useRouter } from 'vue-router'
 
-  import BdgCsvSettings from '@/views/settings/csvSettings/BdgCsvSettings.vue'
+    const router = useRouter()
+    const route = useRoute()
+
+    function navigateBack() {
+        // Check if came from new account page
+        if (route.query.from === 'newAccount') {
+            router.push('/accounts/new')
+        } else {
+            router.go(-1)
+        }
+    }
 </script>
-
