f03f5ae67b644eb72fe96573c0036af7f290d659
Danny T <cobreti_@hotmail.com>
2025-05-24 21:57:37 -0400

    added a button to go back to the accounts list

--- src/components/account/BdgAccountHeader.vue
+++ src/components/account/BdgAccountHeader.vue
@@ -2,6 +2,11 @@
     <v-toolbar>
         <div class="dflex w-100 flex-row flex-content justify-space-between">
             <div class="mr-4 ml-4 d-flex flex-row">
+                <div class="back-button mr-2">
+                    <a @click="goBack" title="Go back">
+                        <v-icon size="Large" icon="mdi-chevron-left"></v-icon>
+                    </a>
+                </div>
                 <div class="header-title">
                     <div>account</div>
                 </div>
@@ -46,12 +51,28 @@
     .account-id {
         font-size: smaller;
     }
+
+    .back-button {
+        display: flex;
+        align-items: center;
+    }
+
+    a {
+        cursor: pointer;
+    }
 </style>
 
 <script setup lang="ts">
     import type { BankAccount } from '@models/BankAccountTypes'
+    import { useRouter } from 'vue-router'
 
     defineProps<{
         bankAccount: BankAccount
     }>()
+
+    const router = useRouter()
+
+    function goBack() {
+        router.go(-1)
+    }
 </script>
